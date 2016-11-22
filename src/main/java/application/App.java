334c881d4bfa43475f34a7c9de917fc69d45912b
elebard2 <angry.n.gineer@gmail.com>
2016-11-22 12:16:42 +0100

    22-11-2016 12:16PM

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -47,40 +47,30 @@ public class App {
 			formation.setDuration(entries.get(i).getFormationDuration());
 			formation.setFormationprovider(entries.get(i).getFormationProvider());
 
-			EmFactory.transaction(e -> {
+			Formation formationBdd = EmFactory.transaction(e -> {
 
-				TypedQuery<Formation> query = e.createQuery("SELECT F FROM Formation F WHERE F.title=:title",
+				TypedQuery<Formation> query = e.createQuery(
+						"SELECT f FROM Formation f WHERE f.title=:title AND f.location = :location AND f.expectedStartingDate ="
+								+ ":expectedStartingDate AND f.realStartingDate ="
+								+ ":realStartingDate and f.duration =:duration AND"
+								+ " f.formationprovider = :formationprovider",
 						Formation.class);
 
-
-				/**
-				 * AND f.location = :location AND f.expectedStartingDate =
-				 * :expectedStartingDate AND f.realStartingDate =
-				 * :realStartingDate and f.duration =:duration AND
-				 * f.formationprovider = :formationprovider
-				 */
-
-				System.out.println(formation.getTitle());
-
-				query = query.setParameter("title", formation.getTitle());
+				query = query.setParameter("title", formation.getTitle())
+						.setParameter("location", formation.getLocation())
+						.setParameter("expectedStartingDate", formation.getExpectedStartingDate())
+						.setParameter("realStartingDate", formation.getRealStartingDate())
+						.setParameter("duration", formation.getDuration())
+						.setParameter("formationprovider", formation.getFormationprovider());
 
 				List<Formation> resultlist = (List<Formation>) query.getResultList();
 
-				/**
-				 * .setParameter("location", formation.getLocation())
-				 * .setParameter("expectedStartingDate",
-				 * formation.getExpectedStartingDate()).setParameter("realStartingDate",
-				 * formation.getRealStartingDate()) .setParameter("duration",
-				 * formation.getDuration()) .setParameter("formationprovider",
-				 * formation.getFormationprovider())
-				 */
-
-				System.out.println("result : " + resultlist);
-
 				if (resultlist.isEmpty()) {
 					e.persist(formation);
+					return formation;
 				}
-				return null;
+
+				return resultlist.get(0);
 
 			});
 
@@ -89,10 +79,48 @@ public class App {
 			employee.setFirstName(entries.get(i).getEmployee().getFirstName());
 			employee.setLastName(entries.get(i).getEmployee().getLastName());
 
+			Employee employeeBdd = EmFactory.transaction(e -> {
+
+				TypedQuery<Employee> query = e.createQuery(
+						"SELECT e FROM Employee e WHERE e.agenceID=:agenceID AND e.firstName = :firstName AND e.lastName ="
+								+ ":lastName",
+						Employee.class);
+
+				query = query.setParameter("agenceID", employee.getAgenceID())
+						.setParameter("firstName", employee.getFirstName())
+						.setParameter("lastName", employee.getLastName());
+
+				List<Employee> resultlist = (List<Employee>) query.getResultList();
+
+				if (resultlist.isEmpty()) {
+					e.persist(employee);
+					return employee;
+				}
+				return resultlist.get(0);
+			});
+
 			FormationRequest formationrequest = new FormationRequest();
-			formationrequest.setEmployee(employee);
-			formationrequest.setFormation(formation);
-			
+			formationrequest.setEmployee(employeeBdd);
+			formationrequest.setFormation(formationBdd);
+
+			EmFactory.transaction(e -> {
+
+				TypedQuery<FormationRequest> query = e.createQuery(
+						"SELECT fr FROM FormationRequest fr WHERE fr.employee=:employee AND fr.formation = :formation",
+						FormationRequest.class);
+
+				query = query.setParameter("employee", formationrequest.getEmployee())
+						.setParameter("formation", formationrequest.getFormation());
+
+				List<FormationRequest> resultlist = (List<FormationRequest>) query.getResultList();
+
+				if (resultlist.isEmpty()) {
+					e.persist(formationrequest);
+				}
+				return null;
+
+			});
+
 		}
 
 		/** EmFactory.getInstance().close(); */
--- src/main/java/entities/FormationRequest.java
+++ src/main/java/entities/FormationRequest.java
@@ -8,14 +8,14 @@ import javax.persistence.ManyToOne;
 
 @Entity
 public class FormationRequest {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer formationRequestID =null;
-	
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer formationRequestID = null;
+
 	@ManyToOne
 	Employee employee;
-	
+
 	@ManyToOne
 	Formation formation;
 
@@ -34,8 +34,8 @@ public class FormationRequest {
 	public void setFormation(Formation formation) {
 		this.formation = formation;
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		return this.employee + " " + this.formationRequestID;
 	}
 
